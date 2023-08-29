672c34a120d57f6edd3cf8ba2f119f0bdd736b89
Pascal <pascal.pater@gmail.com>
2023-08-29 16:08:30 +0200

    Check the filtered events and render on page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,44 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 function FilteredEventsPage() {
-  return <div>Filtered Events Page</div>;
+  const router = useRouter ();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  // Transform year and month to a number by using a +, because in the url array it is a string
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  // Check if it is not a number, for example when someone types in a word in the url manually
+  // also the range of year and month
+  if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+    return <p>Invalid filter, please adjust your values and try again.</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth
+  })
+
+  // Check if filteredEvents is is somehow falsy or the array is empty
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>
+  }
+
+  return (
+  <>
+    <h1>Filtered Events</h1>
+    <EventList items={filteredEvents} />
+  </>
+  )
 }
 export default FilteredEventsPage;
